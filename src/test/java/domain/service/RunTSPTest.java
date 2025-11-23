706d0249aa2d32122c89babb7cb7afea5abe820c
Enzo Dos Anjos <43908457+enzo-dosanjos@users.noreply.github.com>
2025-11-23 23:07:37 +0100

    Setup Junit for unit tests

---
+++ src/test/java/domain/service/RunTSPTest.java
@@ -0,0 +1,12 @@
+package domain.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class RunTSPTest {
+
+    @Test
+    void checkRunTsp() {
+        RunTSP.main(null);
+    }
+}
