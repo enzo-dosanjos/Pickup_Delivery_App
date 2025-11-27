fd638b8b1a4f78dbac3522c1f37b094d2b5601f9
nosense1234 <you@example.com>
2025-11-27 10:37:55 +0100

    modif ApiService.java and PickupDelivery

--- src/main/java/domain/model/PickupDelivery.java
+++ src/main/java/domain/model/PickupDelivery.java
@@ -7,15 +7,26 @@ import java.util.Map;
 
 public class PickupDelivery {
 
+    private final Map<Long, Request> requests;
     private final Map<Long, List<Request>> requestsPerCourier;
     private long warehouseAddress;
 
     public PickupDelivery() {
+        this.requests = new HashMap<>();
         this.requestsPerCourier = new HashMap<>();
     }
 
     public void addRequest(long courierId, Request request) {
+        requests.put(request.getId(), request);
         requestsPerCourier.computeIfAbsent(courierId, k -> new ArrayList<>()).add(request);
+    }
+
+    public List<Request> getRequestsForCourier(long courierId) {
+        return requestsPerCourier.getOrDefault(courierId, Collections.emptyList());
+    }
+
+    public Map<Long, Request> getRequests() {
+        return requests;
     }
 
     public Map<Long, List<Request>> getRequestsPerCourier() {
